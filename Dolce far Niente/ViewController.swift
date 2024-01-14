2d848ae9decb960eca158fee8e4d2d0b6fe6d1dd
SHIN MIKHAIL <shin.m.dp@gmail.com>
2024-01-14 13:28:40 +0200

    Initial Commit

---
+++ Dolce far Niente/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dolce far Niente
+//
+//  Created by SHIN MIKHAIL on 14.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
